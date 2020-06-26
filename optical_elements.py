b9b027fbdec0df2b6cb35ad49e04804a87980c5b
Gabriella Nutt <gabriellanutt@gabriellas-mbp.attlocal.net>
2020-06-25 17:27:36 -0700

    updated files with test modules

--- optical_elements.py
+++ optical_elements.py
@@ -1,27 +1,23 @@
 import numpy as np
 
 # Half Waveplate function
-# x = rotation angle of HWP
-def HWP(x1):
-    hwp = np.array(([np.cos(x1)**2 - np.sin(x1)**2, 2*np.cos(x1)*np.sin(x1)], [2*np.cos(x1)*np.sin(x1), np.sin(x1)**2 - np.cos(x1)**2])) #half waveplate matrix
-# simplifiy HWP with trig identities
-    return(hwp)
 
-# polarizer with leakage
+def hwp(x1): # make all hwp functions lower case (only classer are upper case)
+    "function of a half wave plate,x = rotation angle of HWP"
+    hwp = np.array(([np.cos(np.deg2rad(2*x1)), 2*np.cos(np.deg2rad(x1))*np.sin(np.deg2rad(x1))], [2*np.cos(np.deg2rad(x1))*np.sin(np.deg2rad(x1)), np.sin(np.deg2rad(x1))**2 - np.cos(np.deg2rad(x1))**2])) #half waveplate matrix
+
+    return(hwp)
+# make into sanwhitch form in the same way as rm compair to above
+
 def pol(n=.1):
+    "polarizer with leakage"
     hp = np.array([[np.sqrt(1-n**2), 0],[0, n]]) #polarizer
     return(hp)
 
-def PHWP(x1,n = .1):
-    hwp = np.array(([np.cos(x1)**2 - np.sin(x1)**2, 2*np.cos(x1)*np.sin(x1)], [2*np.cos(x1)*np.sin(x1), np.sin(x1)**2 - np.cos(x1)**2]))
-    p = pol(n)
-    Thwp = hwp.transpose()
-    PHW = Thwp @ pol(n) @ hwp
-
-# polarizer on rotation mount
 def rm(x1,n=.1):
-    RM = np.array([[np.cos(np.deg2rad(x1)),-np.sin(np.deg2rad(x1))],[np.sin(np.deg2rad(x1)),np.cos(np.deg2rad(x1))]])
+    "polarizer on rotation mount"
+    r = np.array([[np.cos(np.deg2rad(x1)),-np.sin(np.deg2rad(x1))],[np.sin(np.deg2rad(x1)),np.cos(np.deg2rad(x1))]])
     p = pol(n=n)
-    RMT = RM.transpose()
-    R = RM @ p @ RMT
-    return(R)
+    rt = r.transpose()
+    return(r @ p @ rt)
+
