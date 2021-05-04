c37f39098e6d4bc7158f4eab8f18d060553f5bd7
Justin Dressel <dressel@chapman.edu>
2021-05-03 23:16:22 -0700

    Fixed broken interface. Added notebook with nice plots and examples for poster.

--- interferometer.py
+++ interferometer.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 ### pretense with oe
 
-def intensity(theta1,theta3,theta4,theta2min, theta2max,l= np.array([1,0]), theta1error = 0.0, theta3error = 0.0, aeff = 0.1, beff = .1, ceff = .1, deff = 0.0):
+def intensity(theta1,theta3,theta4,theta2min,theta2max, l= np.array([1, 0]), theta1error = 0.0, theta3error = 0.0, aeff = 0.1, beff = .1, ceff = .1, deff = 0.0):
     
     """
     the intensity function represents how the intensity of the laser light changes as it goes through the interferometer set up.
@@ -50,7 +50,7 @@ def intensity(theta1,theta3,theta4,theta2min, theta2max,l= np.array([1,0]), thet
     cccw = oe.hwp(theta3+theta3error) @ oe.pol(n=ceff) @ oe.hwp(-theta3) # polarizer 3 with light coming in at a counterclockwise direction
     d = oe.rm(theta4,n= deff) # perfect polarizer between exiting light and detector
     # leave a space here because x2s defining array and above variables defining matrices
-    theta2s = np.arange(theta2min,theta2max,.1) # making an array of all the different rotation angles of polarizer between set max and min angles
+    theta2s = np.arange(theta2min,theta2max,.01) # making an array of all the different rotation angles of polarizer between set max and min angles
 
     @np.vectorize # even though give and return one value its going to take finc and make it work on an array of values
     def darkport_intensity(theta2):
@@ -58,18 +58,18 @@ def intensity(theta1,theta3,theta4,theta2min, theta2max,l= np.array([1,0]), thet
         
         bcw = oe.rm(theta2,n=beff) # polarizer 2 with light coming in at a clockwise direction
         bccw = oe.rm(-theta2,n=beff) # polarizer 2 with light coming in at a counterclockwise direction
-        b = ((accw@ bccw@ cccw-ccw@ bcw@ acw)/2)
+        b = ((accw @ bccw @ cccw) - (ccw @ bcw @ acw))/2
         a = b.transpose()
-        c = (l@(a) @d @ (b)@ l)
-        return (c)
+        c = np.dot(l, a @ d @ b @ l)
+        return(c)
     
     
     return(theta2s,darkport_intensity(theta2s))
 
 
-def vertical(theta1, theta3, theta2min, theta2max, ymax=0.2,aeff = aeff ,beff = beff ,ceff = ceff, deff = deff):
+def vertical(theta1, theta3, theta2min, theta2max, ymax=0.2, aeff = 0.1, beff = .1, ceff = .1, deff = 0.0):
     "this plots the the vertical light coming out of the dark port"
-    x2s,Vt = intensity(theta1,theta3,90,theta2min,theta2max,aeff,beff,ceff,deff)
+    x2s,Vt = intensity(theta1,theta3,90,theta2min,theta2max)
     f=plt.figure(figsize=(4,3))
 
     plt.plot(x2s,Vt,color="purple")
@@ -78,15 +78,15 @@ def vertical(theta1, theta3, theta2min, theta2max, ymax=0.2,aeff = aeff ,beff = 
     plt.title('Vertical Dark Port Intensity vs Polarizer Angle')
     plt.grid(True)
     plt.xlim((theta2min,theta2max))
-    plt.ylim((0,ymax))
+    #plt.ylim((0,ymax))
     plt.show()
     
 
 
 
-def horizontal(theta1,theta3,theta2min, theta2max, ymax=0.2,aeff = aeff,beff = beff ,ceff = ceff,deff = deff ):
+def horizontal(theta1,theta3,theta2min, theta2max, ymax=0.2, aeff = 0.1, beff = .1, ceff = .1, deff = 0.0):
     "this plots the horizontal light coming out of the darkport"
-    x2s,Hz = intensity(theta1,theta3,0, theta2min, theta2max,aeff,beff,ceff,deff)
+    x2s,Hz = intensity(theta1,theta3,0, theta2min, theta2max)
     f=plt.figure(figsize=(4,3))
 
     plt.plot(x2s,Hz,color="magenta")
@@ -95,20 +95,20 @@ def horizontal(theta1,theta3,theta2min, theta2max, ymax=0.2,aeff = aeff,beff = b
     plt.title("Horizontal Dark Port Intensity vs Polarizer Angle")
     plt.grid(True)
     plt.xlim((theta2min,theta2max))
-    plt.ylim((0,ymax))
+    #plt.ylim((0,ymax))
     plt.show()
 
 
 
-def split(theta1,theta3,theta2min, theta2max,aeff = aeff,beff = beff ,ceff = ceff,deff = deff):
+def split(theta1,theta3,theta2min, theta2max, aeff = 0.1, beff = .1, ceff = .1, deff = 0.0):
     "bright port minus dark ratio to total initial intensity"
 
-    x2s,Vt = intensity(theta1,theta3,90,theta2min, theta2max,aeff,beff,ceff,deff)
-    x2s,Hz = intensity(theta1,theta3,0,theta2min, theta2max,aeff,beff,ceff,deff)
+    x2s,Vt = intensity(theta1,theta3,90,theta2min, theta2max)
+    x2s,Hz = intensity(theta1,theta3,0,theta2min, theta2max)
     s = Vt-Hz / (Hz+Vt)
     f=plt.figure(figsize=(4,3))
 
-    plt.plot(x2s,s,color="aqua")
+    plt.plot(x2s,s,color="red")
     plt.xlabel(r'$\phi$ (deg)')
     plt.ylabel(r'$(I_v-I_h)/(I_h+I_v)$')
     plt.title('Dark Port Intensity Difference')
